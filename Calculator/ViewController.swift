7f38fdbe398d6045eb2712f34974471f7149504d
Kadeem Palacios <Main_Home@Kadeems-MacBook-Pro.local>
2016-12-15 13:12:46 -0800

    Implementation of persisted calculation 80% complete

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -19,8 +19,9 @@ class ViewController: UIViewController {
     let defaults = UserDefaults.standard
     var caseCounter = 0
     var digit:String? = nil
-    var digit2 = nil
-    var result:Int = 0
+    var digit2:String? = nil
+
+    var userIsInTheMiddleOfTypingANumber = false
     
     //MARK:- Functions
     func multiply(value1:Int, value2:Int) -> Int {
@@ -44,19 +45,37 @@ class ViewController: UIViewController {
         return result
     }
     func save() {
-        defaults.set(digit, forKey: "textField")
+        defaults.set(digit, forKey: "digit1")
+        if digit2 != nil {
+            self.defaults.set(digit2, forKey: "digit2")
+
+        }
         
     }
     func clear() {
         numberDisplay.text = ""
-        digit = ""
+
+    }
+    func clearAll() {
+        clear()
+         digit = nil
+         digit2 = ""
+        save()
     }
     
     func displayResult(result:String) {
         self.numberDisplay.text! = String(result)
     }
     func fetch() {
-        digit = defaults.string(forKey: "textField")
+        digit = defaults.string(forKey: "digit1")
+        digit2 = defaults.string(forKey: "digit2")
+    }
+    
+    func swap() {
+        if digit2 != nil{
+        digit = digit2
+        digit2 = nil
+        }
     }
     //MARK:- IB Outlets
     
@@ -66,37 +85,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var subtractButton: UIButton!
     @IBOutlet weak var multiplyButton: UIButton!
     @IBOutlet weak var divideButton: UIButton!
+    
     //MARK:- IB Actions
     
     @IBAction func numberTapped(_ sender: UIButton) {
         if digit == nil {
+            
             digit = sender.currentTitle!
-            numberDisplay.text! = digit!
             save()
+            if userIsInTheMiddleOfTypingANumber == true {
+                numberDisplay.text! += digit!
+            }
+            else {
+            numberDisplay.text! = digit!
+            }
         }
-        else { digit2 = sender.currentTitle!
-            numberDisplay.text! = digit2
-            
-            
+        else {
+            digit2 = sender.currentTitle!
+            save()
+            if userIsInTheMiddleOfTypingANumber  == true{
+                numberDisplay.text! += digit2!
+            }
+            else {
+                numberDisplay.text! = digit2!
+            }
         }
-        
+    }
+    @IBAction func ACbutton(_ sender: UIButton) {
+        clearAll()
     }
     @IBAction func Divide(_ sender: UIButton) {
+        swap()
         save()
         clear()
         caseCounter = 1
     }
     @IBAction func Multiply(_ sender: UIButton) {
+        swap()
         save()
         clear()
         caseCounter = 2
     }
     @IBAction func Subtract(_ sender: UIButton) {
+        swap()
         save()
         clear()
         caseCounter = 3
     }
     @IBAction func Add(_ sender: UIButton) {
+        swap()
         save()
         clear()
         caseCounter = 4
@@ -106,10 +143,10 @@ class ViewController: UIViewController {
         
         print("Equals button pressed")
         switch caseCounter{
-        case 1: divide(value1: Int(digit!)!, value2: Int(digit2)!); print("divide function exectuted");
-        case 2: multiply(value1: Int(digit!)!, value2: Int(digit2)!);print("multiply function exectuted");
-        case 3: subtract(value1: Int(digit!)!, value2: Int(digit2)!);print("subtract function exectuted");
-        case 4: add(value1: Int(digit!)!, value2: Int(digit2)!);print("add function exectuted");
+        case 1: divide(value1: Int(digit!)!, value2: Int(digit2!)!); print("divide function exectuted");
+        case 2: multiply(value1: Int(digit!)!, value2: Int(digit2!)!);print("multiply function exectuted");
+        case 3: subtract(value1: Int(digit!)!, value2: Int(digit2!)!);print("subtract function exectuted");
+        case 4: add(value1: Int(digit!)!, value2: Int(digit2!)!);print("add function exectuted");
         default : print ("default case executed")
         }
         
