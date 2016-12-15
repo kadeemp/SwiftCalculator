b22b1e2123cbab5bf5585bf1b68062330942a779
Kadeem Palacios <Main_Home@Kadeems-MacBook-Pro.local>
2016-12-15 11:28:00 -0800

    Basic functionalities working with a few bugs unaccounted for

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,113 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK:- View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        numberDisplay.text = "0"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK:- Variables
+    let defaults = UserDefaults.standard
+    var caseCounter = 0
+    var digit:String? = nil
+    var digit2 = nil
+    var result:Int = 0
+    
+    //MARK:- Functions
+    func multiply(value1:Int, value2:Int) -> Int {
+        let result = value1*value2
+        displayResult(result: String(result))
+        return result
     }
-
-
+    func add(value1:Int, value2:Int) -> Int {
+        let result = value1+value2
+        displayResult(result: String(result))
+        return result
+    }
+    func divide(value1:Int, value2:Int) -> Int {
+        let result = value1/value2
+        displayResult(result: String(result))
+        return result
+    }
+    func subtract(value1:Int, value2:Int) -> Int {
+        let result = value1-value2
+        displayResult(result: String(result))
+        return result
+    }
+    func save() {
+        defaults.set(digit, forKey: "textField")
+        
+    }
+    func clear() {
+        numberDisplay.text = ""
+        digit = ""
+    }
+    
+    func displayResult(result:String) {
+        self.numberDisplay.text! = String(result)
+    }
+    func fetch() {
+        digit = defaults.string(forKey: "textField")
+    }
+    //MARK:- IB Outlets
+    
+    @IBOutlet weak var numberDisplay: UILabel!
+    @IBOutlet weak var equalsButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    //MARK:- IB Actions
+    
+    @IBAction func numberTapped(_ sender: UIButton) {
+        if digit == nil {
+            digit = sender.currentTitle!
+            numberDisplay.text! = digit!
+            save()
+        }
+        else { digit2 = sender.currentTitle!
+            numberDisplay.text! = digit2
+            
+            
+        }
+        
+    }
+    @IBAction func Divide(_ sender: UIButton) {
+        save()
+        clear()
+        caseCounter = 1
+    }
+    @IBAction func Multiply(_ sender: UIButton) {
+        save()
+        clear()
+        caseCounter = 2
+    }
+    @IBAction func Subtract(_ sender: UIButton) {
+        save()
+        clear()
+        caseCounter = 3
+    }
+    @IBAction func Add(_ sender: UIButton) {
+        save()
+        clear()
+        caseCounter = 4
+    }
+    @IBAction func Equals(_ sender: UIButton) {
+        fetch()
+        
+        print("Equals button pressed")
+        switch caseCounter{
+        case 1: divide(value1: Int(digit!)!, value2: Int(digit2)!); print("divide function exectuted");
+        case 2: multiply(value1: Int(digit!)!, value2: Int(digit2)!);print("multiply function exectuted");
+        case 3: subtract(value1: Int(digit!)!, value2: Int(digit2)!);print("subtract function exectuted");
+        case 4: add(value1: Int(digit!)!, value2: Int(digit2)!);print("add function exectuted");
+        default : print ("default case executed")
+        }
+        
+        
+        
+        
+    }
 }
 
